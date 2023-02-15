1e65465ed22b3ed82fa668923a88842d0a4f53bc
Moeez Abdul <MoeezRajput2000@Gmail.com>
2023-02-15 12:22:51 +0000

    WeatherData class populated.

--- app.cpp
+++ app.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <list>
 
+using namespace std;
+
 class Observer
 {
 public:
@@ -16,7 +18,38 @@ public:
 class WeatherData
 {
 public:
+    void registerObserver(Observer *o)
+    {
+        observers.push_back(0);
+    }
+
+    void removeObserver(Observer *o)
+    {
+        observers.remove(o);
+    }
+
+    void notifyObservers()
+    {
+        for (auto o : observers)
+        {
+            /* code */
+            o->update(temperature, humidity, pressure);
+        }
+    }
+
+    void setMeasurements(double temperature, double humidity, double pressure)
+    {
+        this->temperature = temperature;
+        this->humidity = humidity;
+        this->pressure = pressure;
+        notifyObservers();
+    }
+
 private:
+    list<Observer *> observers;
+    double temperature;
+    double humidity;
+    double pressure;
 };
 
 class CurrentConditionDisplay : public Observer, public Display
